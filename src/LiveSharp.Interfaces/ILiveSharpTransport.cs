b51e9491e1f744b637dd1a22bbc2989f528405c9
ionoy <mihhail.maslakov@gmail.com>
2020-05-11 12:13:37 +0300

    added ILiveSharpTransport interface

---
+++ src/LiveSharp.Interfaces/ILiveSharpTransport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LiveSharp.Interfaces
+{
+    public interface ILiveSharpTransport
+    {
+        object ConnectionObject { get; }
+
+        Task Connect(string host, int port, Action<Exception, object> onTransportException);
+        void Send(byte[] buffer);
+        void StartReceiving(Action<object, byte[], int> onBufferReceived);
+        void CloseConnection();
+    }
+}
